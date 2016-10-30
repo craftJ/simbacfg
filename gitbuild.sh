10d9de4e325fc4be90a9b29b827939d84173acc7
craftJ <whiteday_dreamer@163.com>
2016-10-30 20:28:31 +0800

    add git init cmd

---
+++ gitbuild.sh
@@ -0,0 +1,26 @@
+
+# for build a prj git repository
+
+# for ex:https://github.com/craftJ/simbapy
+
+
+…or create a new repository on the command line
+
+echo "# simbapy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/craftJ/simbapy.git
+git push -u origin master
+
+
+
+…or push an existing repository from the command line
+
+git remote add origin https://github.com/craftJ/simbapy.git
+git push -u origin master
+
+
+
+
+
